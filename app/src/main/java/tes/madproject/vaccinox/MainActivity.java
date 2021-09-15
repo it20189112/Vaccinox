cf54e4b323d7e27b64e2392d61870785a517f8f7
Sayuru Jithsara <it20189112@my.sliit.lk>
2021-09-15 20:46:34 +0530

    Added test toast

--- app/src/main/java/tes/madproject/vaccinox/MainActivity.java
+++ app/src/main/java/tes/madproject/vaccinox/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Toast.makeText(this, "Test", toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
     }
 }
