37b18471bb1d54fc9245831061dd7ec7ed0a3a05
Sayuru Jithsara <it20189112@my.sliit.lk>
2021-09-15 20:45:31 +0530

    Added test toast

--- app/src/main/java/tes/madproject/vaccinox/MainActivity.java
+++ app/src/main/java/tes/madproject/vaccinox/MainActivity.java
@@ -3,6 +3,7 @@ package tes.madproject.vaccinox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "Test", toast.LENGTH_SHORT).show();
     }
 }
